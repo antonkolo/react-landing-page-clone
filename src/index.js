4097fcabc7659758bcc9baeb68b1531693d013ee
Anton Kolo <kvanton7@gmail.com>
2024-09-25 16:10:25 +0200

    Fix preflight issues

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import '@fontsource-variable/noto-sans-jp';
+import '@fontsource/pt-serif-caption';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import styles from './index.module.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
